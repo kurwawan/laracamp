0725694624eaa58c8456cd2c281d8f89f21a01ed
kurwawan <kurniawanridwansurohardjo@gmail.com>
2024-08-31 20:34:24 +0700

    seeders

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Seeder;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        // todo: 1st method
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // }
+        // todo: 2nd method
+        Camp::insert($camps);
+    }
+}
